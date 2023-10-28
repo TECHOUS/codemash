f55f062dda47477ce92f5ea32f387c1999f40d05
GauravWalia19 <gauravwalia019@gmail.com>
2023-10-29 02:18:20 +0530

    added the main landing page

---
+++ src/components/landingSection/top/LandingSectionTop.jsx
@@ -0,0 +1,17 @@
+import './LandingSectionTop.css';
+
+const LandingSectionTop = () => {
+    return (
+        <div id="landing-section-top">
+            <h1>Rate the Open Source contributed code</h1>
+            <h2>
+                “Most software today is very much like an Egyptian pyramid with
+                millions of bricks piled on top of each other, with no
+                structural integrity, but just done by bruteforce and thousands
+                of slave” -Alan Kay
+            </h2>
+        </div>
+    );
+};
+
+export default LandingSectionTop;
