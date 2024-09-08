a3121d477dfb672e27b67db564a734d4308bae25
GauravWalia19 <gauravwalia019@gmail.com>
2024-09-08 11:56:59 +0530

    moved the state to the app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,20 @@
 import Header from '../Header';
 import LandingSection from '../LandingSection';
 import './App.css';
+import { useState } from 'react';
+import RatingPopup from '../RatingPopup';
 
 function App() {
+    const [isPopupOpened, setIsPopupOpened] = useState(false);
+
     return (
         <>
             <Header />
-            <LandingSection />
+            <LandingSection
+                isPopupOpened={isPopupOpened}
+                setIsPopupOpened={setIsPopupOpened}
+            />
+            {isPopupOpened ? <RatingPopup /> : <></>}
         </>
     );
 }
--- src/components/LandingSection/LandingSection.jsx
+++ src/components/LandingSection/LandingSection.jsx
@@ -2,13 +2,9 @@ import LandingSectionTop from './top/LandingSectionTop.jsx';
 import LandingSectionMid from './mid/LandingSectionMid.jsx';
 import LandingSectionBottom from './bottom/LandingSectionBottom.jsx';
 import './LandingSection.css';
-import { useState } from 'react';
-import RatingPopup from './RatingPopup/RatingPopup.jsx';
-import DotSpinner from '../DotSpinner/DotSpinner.jsx';
+import PropTypes from 'prop-types';
 
-const LandingSection = () => {
-    const [isPopupOpened, setIsPopupOpened] = useState(false);
-
+const LandingSection = ({ isPopupOpened, setIsPopupOpened }) => {
     return (
         <div id="landing-section">
             <div id="landing-section-content">
@@ -18,11 +14,14 @@ const LandingSection = () => {
                     setIsPopupOpened={setIsPopupOpened}
                 />
                 <LandingSectionBottom />
-                <DotSpinner dotCount={5} />
-                {isPopupOpened ? <RatingPopup /> : <></>}
             </div>
         </div>
     );
+};
+
+LandingSection.propTypes = {
+    isPopupOpened: PropTypes.bool,
+    setIsPopupOpened: PropTypes.func,
 };
 
 export default LandingSection;
