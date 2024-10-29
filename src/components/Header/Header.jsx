383fbf1d8cb34ecadc5339e7c8478b3ebd9ac26d
GauravWalia19 <gauravwalia019@gmail.com>
2024-10-29 18:32:37 +0530

    1. Fix the codemash default navigation 2. Fixed the Rate and Search header links navigation 3. Fixed the view your rating icon navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,23 @@
 import './Header.css';
 import HeaderImg from './HeaderImg';
 import HeaderLinks from './HeaderLinks';
+import PropTypes from 'prop-types';
 
-const Header = () => {
+const Header = ({ appDispatch, isPopupOpened }) => {
     return (
         <div id="header-outer-div">
             <HeaderImg />
-            <HeaderLinks />
+            <HeaderLinks
+                appDispatch={appDispatch}
+                isPopupOpened={isPopupOpened}
+            />
         </div>
     );
+};
+
+Header.propTypes = {
+    appDispatch: PropTypes.func,
+    isPopupOpened: PropTypes.bool,
 };
 
 export default Header;
--- src/components/Header/HeaderLinks/HeaderLinks.jsx
+++ src/components/Header/HeaderLinks/HeaderLinks.jsx
@@ -1,15 +1,26 @@
 import './HeaderLinks.css';
 import HeaderLink from './HeaderLink';
+import PropTypes from 'prop-types';
 
-const HeaderLinks = () => {
+const HeaderLinks = ({ appDispatch, isPopupOpened }) => {
     return (
         <div className="inline-header" id="desktop-header">
             <ul id="desktop-header-list">
-                <HeaderLink linkName="Rate" href="#" />
-                <HeaderLink linkName="Search" href="#" />
+                <HeaderLink
+                    linkName="Rate"
+                    href="#landing-section"
+                    appDispatch={appDispatch}
+                    isPopupOpened={isPopupOpened}
+                />
+                <HeaderLink linkName="Search" href="#searchSection" />
             </ul>
         </div>
     );
+};
+
+HeaderLinks.propTypes = {
+    appDispatch: PropTypes.func,
+    isPopupOpened: PropTypes.bool,
 };
 
 export default HeaderLinks;
