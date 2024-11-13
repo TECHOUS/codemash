ef45efce098462c48c02bf631b93ad1d46df9f70
GauravWalia19 <gauravwalia019@gmail.com>
2024-11-13 23:03:00 +0530

    added the github pages secrets and fixed the image

--- src/components/Header/HeaderImg/HeaderImg.jsx
+++ src/components/Header/HeaderImg/HeaderImg.jsx
@@ -4,7 +4,7 @@ const HeaderImg = () => {
     return (
         <div className="inline-header" id="header-img-div">
             <a href="#landing-section" id="header-img-link">
-                <img src="/light_logo.png" id="header-img" />
+                <img src="./light_logo.png" id="header-img" />
             </a>
         </div>
     );
